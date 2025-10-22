94535ccae51ccb531d7c83e6064b06d96b68d697
Said Ruíz <mathiassaid7@gmail.com>
2025-10-22 17:26:34 -0500

    Update Header.tsx

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Car, User, Menu, X } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
