efdfdd934a58e7f0c79c28faa5d32b622ff7196e
Lobosanplay <parra.santiago2008@gmail.com>
2025-10-31 14:12:05 -0500

    feat: Create filters of car catalog

--- src/pages/Landing/catalog/Catalog.tsx
+++ src/pages/Landing/catalog/Catalog.tsx
@@ -6,126 +6,149 @@ import { useDispatch, useSelector } from "react-redux";
 import { Search, Luggage, Users, DoorClosed, Gauge, Wind, Plus} from "lucide-react";
 import { Link } from "react-router-dom";
 import { useAuth } from "@/shared/hooks/useAuth";
+import type { SB_CarForRentModel } from "@/shared/models/carForRent/carForRent.model";
 
 export default function Catalog() {
-  const [search, setSearch] = useState("")
-  const { user } = useAuth()
-  // INFO: Un objeto que contiene otros objetos (cars.cars)
-  const cars = useSelector((state: RootState) => state.car);
-  const dispatch = useDispatch<AppDispatch>();
+    const [search, setSearch] = useState("")
+    const { user } = useAuth()
+    // INFO: Un objeto que contiene otros objetos (cars.cars)
+    const cars = useSelector((state: RootState) => state.car);
+    const filters = useSelector((state: RootState) => state.filters);
 
-  useEffect(() => {
-    dispatch(fetchCars());
-  }, [dispatch]);
+    const dispatch = useDispatch<AppDispatch>();
 
-  const seachFilter = cars.cars.filter((car) =>
-    `${car.brand} ${car.group} ${car.year}`
-      .toLowerCase()
-      .includes(search.toLowerCase())
-  );
+    useEffect(() => {
+        dispatch(fetchCars());
+    }, [dispatch]);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value.trimStart());
-  };
+    const applyFilters = (carList: SB_CarForRentModel[]) => {
+        let filteredCars = carList;
 
-  return (
-    <>
-      <div className="flex flex-col items-center mt-15 gap-4 pb-4">
-        <div className="flex items-center w-[80%]">
-            <input 
-                type="text"
-                placeholder="Escribe el modelo o marca del carro..."
-                className="flex-1 px-2 py-3 bg-white rounded-l-lg focus:outline-none"
-                value={search}
-                onChange={handleChange}
-            />
-            {!user ? (
-                <div 
-                    className="flex items-center justify-center px-4 bg-[var(--blue-tertiary)] text-white py-3 rounded-r-lg cursor-pointer">
-                    <Search/>
-                </div>
-            ) : (
-                <div className="flex">
-                    <div className="flex gap-5 items-center justify-center px-4 bg-[var(--blue-tertiary)] text-white py-3 border-r cursor-pointer">
+        if (search) {
+            filteredCars = filteredCars.filter((car) => 
+                `${car.brand} ${car.group} ${car.year}`
+                .toLowerCase()
+                .includes(search.toLowerCase())
+            )
+        };
+
+        if (filters.pick_up_location) {
+            filteredCars = filteredCars.filter((car) => 
+                car.pick_up_location.toLowerCase().includes(filters.pick_up_location.toLowerCase()))
+        };
+        return filteredCars;
+    }
+
+    const filteredCars = applyFilters(cars.cars);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value.trimStart());
+    };
+
+    useEffect(() => {
+        if (filters.pick_up_location || filters.date_collection) {
+        console.log("Filtros aplicados:", filters);
+        }
+    }, [filters]);
+
+    return (
+        <>
+        <div className="flex flex-col items-center mt-15 gap-4 pb-4">
+            <div className="flex items-center w-[80%]">
+                <input 
+                    type="text"
+                    placeholder="Escribe el modelo o marca del carro..."
+                    className="flex-1 px-2 py-3 bg-white rounded-l-lg focus:outline-none"
+                    value={search}
+                    onChange={handleChange}
+                />
+                {!user ? (
+                    <div 
+                        className="flex items-center justify-center px-4 bg-[var(--blue-tertiary)] text-white py-3 rounded-r-lg cursor-pointer">
                         <Search/>
                     </div>
-                    <Link to='/addcar'>
-                        <button className="flex gap-5 items-center justify-center px-4 bg-[var(--red-quartenary)] text-white py-3 rounded-r-lg cursor-pointer">
-                            <Plus /> 
-                            <span>Alquila tu auto</span>
-                        </button>
-                    </Link>
-                </div>
-            )}
-        </div>
-        
-        <div className="w-[80%] grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3">
-             {seachFilter.length > 0 ? (
-                seachFilter.map((car) => (
-                    <div key={car.id} className="cardCar bg-white rounded-lg shadow-md shadow-black/20 overflow-hidden hover:shadow-lg transition-all duration-300 hover:-translate-y-1 animate-fade-in-up">
-                        <div className="rounded-t-lg relative w-full h-48 overflow-hidden">
-                            <span className="absolute right-2 top-2 bg-[var(--yellow-secondary)] text-sm px-2 py-1 rounded-lg z-90">
-                                ${car.rent_per_day}
-                            </span>
-                            <img
-                                src={car.urls_img[0]}
-                                className="object-cover h-full w-full rounded-t-lg transition-transform duration-300 hover:scale-110 select-none"
-                            />
+                ) : (
+                    <div className="flex">
+                        <div className="flex gap-5 items-center justify-center px-4 bg-[var(--blue-tertiary)] text-white py-3 border-r cursor-pointer">
+                            <Search/>
                         </div>
-
-                        <div className="flex flex-col gap-2 px-2 py-3">
-                            <div className="flex flex-col">
-                                <span className="font-semibold text-lg">
-                                    Grupo {car.group}
+                        <Link to='/addcar'>
+                            <button className="flex gap-5 items-center justify-center px-4 bg-[var(--red-quartenary)] text-white py-3 rounded-r-lg cursor-pointer">
+                                <Plus /> 
+                                <span>Alquila tu auto</span>
+                            </button>
+                        </Link>
+                    </div>
+                )}
+            </div>
+            
+            <div className="w-[80%] grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3">
+                {filteredCars.length > 0 ? (
+                    filteredCars.map((car) => (
+                        <div key={car.id} className="cardCar bg-white rounded-lg shadow-md shadow-black/20 overflow-hidden hover:shadow-lg transition-all duration-300 hover:-translate-y-1 animate-fade-in-up">
+                            <div className="rounded-t-lg relative w-full h-48 overflow-hidden">
+                                <span className="absolute right-2 top-2 bg-[var(--yellow-secondary)] text-sm px-2 py-1 rounded-lg z-90">
+                                    ${car.rent_per_day}
                                 </span>
-                                <span className="text-md text-gray-600">{car.brand}</span>
+                                <img
+                                    src={car.urls_img[0]}
+                                    className="object-cover h-full w-full rounded-t-lg transition-transform duration-300 hover:scale-110 select-none"
+                                />
                             </div>
 
-                            <div className="flex flex-wrap gap-4 text-gray-600 items-center">
-                                <div className="flex items-center gap-1">
-                                    <Users size={18} />
-                                    <span className="text-md">{car.passenger_capacity}</span>
+                            <div className="flex flex-col gap-2 px-2 py-3">
+                                <div className="flex flex-col">
+                                    <span className="font-semibold text-lg">
+                                        Grupo {car.group}
+                                    </span>
+                                    <span className="text-md text-gray-600">{car.brand}</span>
                                 </div>
 
-                            <div className="flex items-center gap-1">
-                                <Luggage size={18} />
-                                <span className="text-md">{car.luggage_capacity}</span>
-                            </div>
-
-                            <div className="flex items-center gap-1">
-                                <Gauge size={18} />
-                                <span className="text-md">{car.transmission}</span>
-                            </div>
-
-                            <div className="flex items-center gap-1">
-                                <DoorClosed size={18} />
-                                <span className="text-md">{car.car_doors}</span>
+                                <div className="flex flex-wrap gap-4 text-gray-600 items-center">
+                                    <div className="flex items-center gap-1">
+                                        <Users size={18} />
+                                        <span className="text-md">{car.passenger_capacity}</span>
                                     </div>
 
-                            <div className="flex items-center gap-1">
-                                <Wind size={18} />
-                                <span className="text-md">
-                                {car.air_conditioning ? "Sí" : "No"}
-                                </span>
-                            </div>
-                            </div>
+                                <div className="flex items-center gap-1">
+                                    <Luggage size={18} />
+                                    <span className="text-md">{car.luggage_capacity}</span>
+                                </div>
 
-                            <Link 
-                                to={`/vehicles/${car.id}`}
-                                className=" flex w-full py-2 bg-[var(--yellow-secondary)] font-semibold rounded-lg cursor-pointer items-center justify-center"
-                            >
-                            Ver detalles
-                            </Link>
-                        </div>
-                        </div>
-                    ))
-                    ) : (
-                    <span className="col-span-full text-center text-gray-600 py-6">
-                        No se encontraron carros
-                    </span>
-                )}
+                                <div className="flex items-center gap-1">
+                                    <Gauge size={18} />
+                                    <span className="text-md">{car.transmission}</span>
+                                </div>
+
+                                <div className="flex items-center gap-1">
+                                    <DoorClosed size={18} />
+                                    <span className="text-md">{car.car_doors}</span>
+                                        </div>
+
+                                <div className="flex items-center gap-1">
+                                    <Wind size={18} />
+                                    <span className="text-md">
+                                    {car.air_conditioning ? "Sí" : "No"}
+                                    </span>
+                                </div>
+                                </div>
+
+                                <Link 
+                                    to={`/vehicles/${car.id}`}
+                                    className=" flex w-full py-2 bg-[var(--yellow-secondary)] font-semibold rounded-lg cursor-pointer items-center justify-center"
+                                >
+                                Ver detalles
+                                </Link>
+                            </div>
+                            </div>
+                        ))
+                        ) : (
+                        <span className="col-span-full text-center text-gray-600 py-6">
+                            No se encontraron carros
+                        </span>
+                    )}
+            </div>
         </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
--- src/pages/Landing/main/sections/Hero/Hero.tsx
+++ src/pages/Landing/main/sections/Hero/Hero.tsx
@@ -1,6 +1,46 @@
 import { Calendar, Clock, MapPin, Search } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useDispatch } from "react-redux";
+import { setFilters } from "@/config/store/slices/filters/filters.slice";
+import { useNavigate } from "react-router-dom";
 
 export default function Hero() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const formHeroSchema = z.object({
+    pick_up_location: z.string().min(1, "La ubicación es requerida"),
+    date_collection: z.string().min(1, "La fecha de recogida es requerida"),
+    hour: z.string().min(1, "La hora es requerida"),
+    date_return: z.string().min(1, "La fecha de devolución es requerida"),
+  })
+
+  type formHero = z.infer<typeof formHeroSchema>;
+
+  const {
+        handleSubmit,
+        formState: { errors },
+        setValue,
+        watch
+    } = useForm<formHero>({
+        resolver: zodResolver(formHeroSchema),
+        defaultValues: {}
+    });
+
+  const formData = watch();
+
+  const handleChange = (field: keyof formHero, value: any) => {
+    setValue(field, value);
+  };
+
+  const onSubmit = (data: formHero) => {
+    console.log('Formulario válido:', data);
+    dispatch(setFilters(data))
+    navigate('/catalog')
+  }
+
   return (
     <section className="relative bg-linear-210 from-[var(--green-primary)] via-[var(--blue-tertiary)] to-[var(--yellow-secondary)] overflow-hidden min-h-[98dvh] flex items-center mt-4">
       <div className="absolute inset-0 h-full w-full  bg-[radial-gradient(#1a67ad90_2px,transparent_0.2px)] [background-size:36px_36px]" />
@@ -14,23 +54,15 @@ export default function Hero() {
                 vive tu próxima aventura
               </span>
             </h1>
-            <div className="inline-flex w-max text-white items-center gap-3 rounded-lg bg-[var(--red-quartenary)] px-4 py-3 animate-scale-in">
-              <div className="text-2xl font-bold text-accent-foreground">
-                15% OFF
-              </div>
-              <div className="text-sm text-accent-foreground">
-                <div className="font-semibold">Usa el cupón BIENVENIDO</div>
-                <div className="text-xs opacity-90">
-                  Válido hasta el 31 de diciembre de 2025
-                </div>
-              </div>
-            </div>
             <div className="text-card-foreground flex flex-col gap-6 rounded-xl border border-slate-200/80 bg-white/20 backdrop-blur-xl p-6 shadow-2xl animate-slide-in-right">
               <div className="space-y-4">
-                <div className="grid gap-4 md:grid-cols-2">
+                <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  className="grid gap-4 md:grid-cols-2"
+                >
                   <div className="space-y-2">
                     <label
-                      htmlFor="city"
+                      htmlFor="pick_up_location"
                       className="text-sm font-medium text-card-foreground"
                     >
                       Lugar de recogida
@@ -38,9 +70,11 @@ export default function Hero() {
                     <div className="relative">
                       <MapPin className="absolute text-neutral-800 bottom-2.5 left-1.5 size-5" />
                       <input
+                        value={formData?.pick_up_location || ""}
+                        onChange={(e) => handleChange("pick_up_location", e.target.value)}
                         type="text"
-                        name="city"
-                        id="city"
+                        name="pick_up_location"
+                        id="pick_up_location"
                         placeholder="Ciudad, Aeropuerto..."
                         className="outline-0 rounded-md placeholder:text-neutral-800 border border-black px-2 pl-8 h-10 w-full"
                       />
@@ -56,6 +90,8 @@ export default function Hero() {
                     <div className="relative">
                       <Calendar className="absolute bottom-2.5 size-5 left-2 text-neutral-800" />
                       <input
+                        value={formData?.date_collection || ""}
+                        onChange={(e) => handleChange("date_collection", e.target.value)}
                         type="date"
                         name="date-collection"
                         id="date-collection"
@@ -65,17 +101,19 @@ export default function Hero() {
                   </div>
                   <div className="space-y-2">
                     <label
-                      htmlFor="time"
+                      htmlFor="hour"
                       className="text-sm font-medium text-card-foreground"
                     >
-                      Hora
+                      Hora de devolución
                     </label>
                     <div className="relative">
                       <Clock className="text-neutral-800 size-5 absolute bottom-2.5 left-2" />
                       <input
+                        value={formData?.hour || ""}
+                        onChange={(e) => handleChange("hour", e.target.value)}
                         type="time"
-                        name="time"
-                        id="time"
+                        name="hour"
+                        id="hour"
                         className="outline-0 rounded-md placeholder:text-neutral-800 border border-black px-2 pl-8 h-10 w-full"
                       />
                     </div>
@@ -90,6 +128,8 @@ export default function Hero() {
                     <div className="relative">
                       <Calendar className="text-neutral-800 absolute size-5 bottom-2.5 left-2" />
                       <input
+                        value={formData?.date_return || ""}
+                        onChange={(e) => handleChange("date_return", e.target.value)}
                         type="date"
                         name="date-return"
                         id="date-return"
@@ -97,11 +137,13 @@ export default function Hero() {
                       />
                     </div>
                   </div>
-                </div>
-                <button className="inline-flex items-center justify-center gap-2 rounded-md transition-all px-4 py-2 w-full bg-[var(--yellow-secondary)] h-12 text-base font-semibold cursor-pointer">
+                <button 
+                  type="submit"
+                  className="inline-flex col-span-2 items-center justify-center gap-2 rounded-md transition-all px-4 py-2 w-full bg-[var(--yellow-secondary)] h-12 text-base font-semibold cursor-pointer">
                   <Search className="size-5" />
                   Buscar Vehículos
                 </button>
+                </form>
               </div>
             </div>
           </div>
